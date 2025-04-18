2fc81961730da00464bb7081e94dcc8bb8dc4138
pjr.monto@gmail.com <pjr.monto@gmail.com>
2025-04-18 13:07:05 +0200

    Unit tests for User class

--- src/test/java/models/TestUsers.java
+++ src/test/java/models/TestUsers.java
@@ -1,12 +1,36 @@
 package models;
 
+import models.User;
+
+import models.board.BoardName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestUsers {
 
     @Test
-    void test(){
+    void testUserCreationWithUsername() {
+        User user = new User("alice");
+        assertNotNull(user.getId());
+    }
 
+    @Test
+    void testUserCreationWithPassword() {
+        User user = new User("bob", "mypassword");
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    void testAddAndDeleteBoard() {
+        User user = new User("charlie");
+        user.addBoard(BoardName.FREE_TIME);
+        user.deleteBoard(BoardName.FREE_TIME);
+    }
+
+    @Test
+    void testDeleteNonExistentBoard() {
+        User user = new User("dave");
+        assertDoesNotThrow(() -> user.deleteBoard(BoardName.WORK));
     }
 }
