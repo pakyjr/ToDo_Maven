b88ced901e24e94eec2354517ab1994cea9da1d6
Maryevans96 <toppimariagrazia@yahoo.it>
2025-05-14 17:27:37 +0200

    Gui form User e ListaBacheche

---
+++ src/main/java/ListaBacheche.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ListaBacheche {
+    private JComboBox comboBox1;
+    private JPanel panel1;
+    private JTextField personalBoardListTextField;
+    private JButton addBoardButton;
+    private JButton deleteBoardButton;
+}
