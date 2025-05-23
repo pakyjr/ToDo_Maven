a8371e929f5c88a4996da0121d660443846d88dc
Maryevans96 <toppimariagrazia@yahoo.it>
2025-05-23 16:16:13 +0200

    userform gui

---
+++ src/main/java/userform.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class userform {
+    private JPanel panel1;
+    private JButton loginButton;
+    private JButton donTHaveAccountButton;
+    private JTextField usernameField1;
+    private JPasswordField passwordField1;
+}
