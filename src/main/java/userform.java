3833ee0eef25377aa1877dcf93e016fa2d674e00
Maryevans96 <toppimariagrazia@yahoo.it>
2025-05-23 18:57:36 +0200

    UserForm e RegisterForm gui. Errore Could not find or load main class UserForm. Capire perchè. Problema controller inesistente??

--- src/main/java/userform.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class userform {
-    private JPanel panel1;
-    private JButton loginButton;
-    private JButton donTHaveAccountButton;
-    private JTextField usernameField1;
-    private JPasswordField passwordField1;
-}
