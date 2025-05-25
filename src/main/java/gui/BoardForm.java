6da885c6297a8c9bcdc6159282dd9e9be15f5ccb
CristianProroga <cristianproroga@gmail.com>
2025-05-25 16:09:57 +0200

    Create link to ToDoForm

--- src/main/java/gui/BoardForm.java
+++ src/main/java/gui/BoardForm.java
@@ -1,10 +1,12 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BoardForm {
     private JPanel board;
-    private JComboBox comboBox1;
+    private JComboBox comboBoxBoards;
     private JButton addBoardButton;
     private JButton shareBoardButton;
     private JButton addToDoButton;
@@ -20,6 +22,19 @@ public class BoardForm {
     frameBoardForm.pack();
         frameBoardForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        this.comboBoxBoards.addItem("My boards");
+        this.comboBoxBoards.addItem("University");
+        this.comboBoxBoards.addItem("Work");
+        this.comboBoxBoards.addItem("Free Time");
 
+
+        addToDoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                ToDoForm toDoForm = new ToDoForm(frameBoardForm);
+                toDoForm.frameToDoForm.setVisible(true);
+            }
+        });
     }
 }
--- src/main/java/gui/ToDoForm.java
+++ src/main/java/gui/ToDoForm.java
@@ -1,9 +1,11 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ToDoForm {
-    private JPanel todopanel;
+    private JPanel todoPanel;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -14,4 +16,23 @@ public class ToDoForm {
     private JTextField textField5;
     private JCheckBox londraCheckBox;
     private JCheckBox milanoCheckBox;
+    private JButton buttonSave;
+    public JFrame frameToDoForm, frame;
+
+    public ToDoForm(JFrame parent){
+        frame = parent;
+
+        frameToDoForm=new JFrame("ToDo Creation");
+        frameToDoForm.setContentPane(todoPanel);
+        frameToDoForm.pack();
+
+        frameToDoForm.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                frame.setVisible(true);
+                frameToDoForm.setVisible(false);
+                frameToDoForm.dispose();
+            }
+        });
+    }
 }
